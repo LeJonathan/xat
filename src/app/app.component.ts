c7f9d0d80787301f65f1dc6cb43e06c185becb4a
LeJon <LeJonVi@outlook.com>
2020-02-26 00:05:47 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fexbot';
+  private headers = new HttpHeaders({ 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' });
+  public commands = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any[]>("assets/data/commands.json").subscribe(
+      (data) => {
+        this.commands = data;
+      }
+    );
+  }
+
+  accClick(t) {
+    var element = (<HTMLElement> t);
+    element.classList.toggle("active");
+    var panel = (<HTMLElement> element.nextElementSibling);
+    if (panel.style.display === "block") {
+        panel.style.display = "none";
+    } else {
+        panel.style.display = "block";
+    }
+  }
 }
